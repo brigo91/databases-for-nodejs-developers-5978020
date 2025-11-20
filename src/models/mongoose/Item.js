de81331972d236ae158da5b626dd4220c822199a
balazs.brigovacz <balazs_brigovacz@epam.com>
2025-11-20 15:44:49 +0100

    Performing text search in MongoDB

--- src/models/mongoose/Item.js
+++ src/models/mongoose/Item.js
@@ -12,4 +12,8 @@ const ItemSchema = new mongoose.Schema(
   }
 );
 
+ItemSchema.index({ tags: 1 });
+ItemSchema.index({ name: 1 });
+ItemSchema.index({ name: 'text' });
+
 export const Item = mongoose.model("Item", ItemSchema);
