d3984bef77d3eb63d949aa754b0d160ec996e189
balazs.brigovacz <balazs_brigovacz@epam.com>
2025-11-21 11:44:06 +0100

    Loading models

--- src/models/sequelize/OrderItem.js
+++ src/models/sequelize/OrderItem.js
@@ -1,29 +1,27 @@
-import { name } from "ejs"
-
 export default (sequelize, DataTypes) => {
-    const OrderItem = sequelize.define("OrderItem", {
-        sku: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        qty: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        price: {
-            type: DataTypes.DECIMAL(10, 2),
-            allowNull: false,
-        }
-    });
-    OrderItem.associate = (models) => {
-        OrderItem.belongsTo(models.Order, {
-            foreignKey: "orderId",
-            as: "Order",
-        });
+  const OrderItem = sequelize.define("OrderItem", {
+    sku: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    qty: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
     }
-    return OrderItem;
-}
+  });
+  OrderItem.associate = (models) => {
+    OrderItem.belongsTo(models.Order, {
+      foreignKey: "orderId",
+      as: "Order"
+    });
+  };
+  return OrderItem;
+};
--- src/plugins/databases/sequelize.js
+++ src/plugins/databases/sequelize.js
@@ -1,5 +1,8 @@
 import fp from "fastify-plugin";
 import { Sequelize } from "sequelize";
+import { readdir } from "fs/promises";
+import { pathToFileURL } from 'url';
+import path from "path";
 
 async function sequelizePlugin(fastify, config) {
   let mysqlStatus = "disconnected";
@@ -11,6 +14,31 @@ async function sequelizePlugin(fastify, config) {
     fastify.log.info("Connection to MySQL has been established successfully.");
     mysqlStatus = "connected";
     fastify.decorate("sequelize", sequelize);
+
+    // Deal with models
+    const models = {};
+    const modelPath = path.resolve("src/models/sequelize");
+    const modelFiles = await readdir(modelPath);
+    for (const file of modelFiles) {
+      if (file.endsWith(".js")) {
+        const filePath = path.join(modelPath, file);
+        const fileUrl = pathToFileURL(filePath).href;
+        const model = (await import(fileUrl)).default(sequelize, Sequelize.DataTypes);
+        models[model.name] = model;
+        fastify.log.info(`Loaded model: ${model.name}`);
+      }
+    }
+
+    Object.values(models).forEach((model) => {
+      if (model.associate) {
+        model.associate(models);
+      }
+    });
+
+    await sequelize.sync({ alter: false });
+    fastify.log.info("Sequelize models were synchronized successfully.");
+
+    fastify.decorate("models", models);
   } catch (err) {
     fastify.log.error("Unable to connect to the MySQL database:", err);
     throw err;
