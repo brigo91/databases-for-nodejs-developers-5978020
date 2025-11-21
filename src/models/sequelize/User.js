df210cd3f9f7a89ed4a1f14b1637a5b9c9e4b277
balazs.brigovacz <balazs_brigovacz@epam.com>
2025-11-21 10:34:14 +0100

    Creating the order models for Sequelize

---
+++ src/models/sequelize/User.js
@@ -0,0 +1,17 @@
+export default (sequelize, DataTypes) => {
+    const User = sequelize.define("User", {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: { type: DataTypes.STRING, allowNull: false },
+    });
+    User.assoxiate = (models) => {
+        User.hasMany(models.Order, { foreignKey: "userId", as: "orders" });
+    }
+    return User;
+};
--- src/plugins/databases/sequelize.js
+++ src/plugins/databases/sequelize.js
@@ -1,15 +1,27 @@
 import fp from "fastify-plugin";
+import { Sequelize } from "sequelize";
 
 async function sequelizePlugin(fastify, config) {
   let mysqlStatus = "disconnected";
 
   // TODO: Connect to MySQL via Sequelize and update the status
+  const sequelize = new Sequelize(config.uri, config.options);
+  try {
+    await sequelize.authenticate();
+    fastify.log.info("Connection to MySQL has been established successfully.");
+    mysqlStatus = "connected";
+    fastify.decorate("sequelize", sequelize);
+  } catch (err) {
+    fastify.log.error("Unable to connect to the MySQL database:", err);
+    throw err;
+  }
   fastify.decorate("mysqlStatus", () => mysqlStatus);
 
   // Graceful shutdown
   fastify.addHook("onClose", async (fastifyInstance, done) => {
     mysqlStatus = "disconnected";
     // TODO: Close Sequelize connection
+    await sequelize.close();
     done();
   });
 }
